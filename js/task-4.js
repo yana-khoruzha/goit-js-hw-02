423839c67fc4116eb10123a560d3b8568f578358
Yana <yana@MacBook-Pro-Yana.local>
2023-11-19 04:03:19 +0200

    changes after review

--- js/task-4.js
+++ js/task-4.js
@@ -1,5 +1,4 @@
 function getShippingCost(country) {
-  let price;
   switch (country) {
     case 'China':
       return `Shipping to ${country} will cost 100 credits`;
@@ -14,9 +13,9 @@ function getShippingCost(country) {
   }
 }
 
-console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
-console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
-console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
-console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
-console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+// console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+// console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+// console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+// console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+// console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+// console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
